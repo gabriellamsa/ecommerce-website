452a4ed0d8dc0195fa96dab51e7418505b3f949f
gabriellamsa <gabii.marcelly@gmail.com>
2025-01-24 17:16:43 -0500

    Add RatingStars

--- src/screen/product/ProductCard.jsx
+++ src/screen/product/ProductCard.jsx
@@ -2,6 +2,25 @@ import { IoMdHeart } from "react-icons/io";
 import { IoCart } from "react-icons/io5";
 import { NavLink } from "react-router-dom";
 import { BodyOne } from "../../components/common/CustomComponents";
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+
+export const RenderRatingStars = (rating) => {
+  const totalStars = 5;
+  const fullStars = Math.floor(rating);
+  const hasHalfStars = rating % 1 !== 0;
+  const stars = [];
+
+  for (let i = 1; i <= totalStars; i++) {
+    if (i <= fullStars) {
+      stars.push(<FaStar key={i} color="#ff8a00" />);
+    } else if (hasHalfStars && i === fullStars + 1) {
+      stars.push(<FaStarHalfAlt key="half-star" color="#ff8a00" />);
+    } else {
+      stars.push(<FaStar key={i} color="#ff8a00" />);
+    }
+  }
+  return stars;
+};
 
 export const ProductCard = ({
   id,
@@ -52,7 +71,9 @@ export const ProductCard = ({
           <NavLink to={`/product-details/${id}`}>
             <BodyOne>{title}</BodyOne>
           </NavLink>
-          <div className="flex items-center gap-2 -mt-2 mb-2">{rating}</div>
+          <div className="flex items-center gap-2 -mt-2 mb-2">
+            {RenderRatingStars(rating)}
+          </div>
           <div className="flex items-center gap-3">
             {price.slice(0, 1).map((priceItem, index) => (
               <>
