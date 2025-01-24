959a63543708caa93c527067352bb40f2d8462f7
gabriellamsa <gabii.marcelly@gmail.com>
2025-01-24 13:44:15 -0500

    Add ProductCard images

--- src/screen/product/ProductCard.jsx
+++ src/screen/product/ProductCard.jsx
@@ -1,3 +1,25 @@
-export const ProductCard = () => {
-  return <div>ProductCard</div>;
+export const ProductCard = ({
+  id,
+  key,
+  title,
+  description,
+  images,
+  price,
+  discount,
+  rating,
+  featured,
+  category,
+  color,
+}) => {
+  return (
+    <>
+      <div className="product card">
+        <div className="images h-96">
+          {images.map((image, index) => (
+            <img key={index} src={image?.image} alt={title} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
