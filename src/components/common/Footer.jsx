93db10a1f2711412f6a833f5a24ac46015dedf60
gabriellamsa <gabii.marcelly@gmail.com>
2025-01-31 15:52:57 -0500

    add contact info and subscription form

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,4 +1,5 @@
 import LogoImg from "../../assets/common/logo.png";
+import { BodyOne, Caption } from "./CustomComponents";
 
 export const Footer = () => {
   return (
@@ -7,6 +8,19 @@ export const Footer = () => {
         <div className="container grid-cols-1 grid md:grid-cols-2 lg:grid-cols-4 gap-8">
           <div>
             <img src={LogoImg} alt="LogoImg" className="h-12" />
+
+            <div className="flex flex-col gap-2 mt-5">
+              <Caption>Address : 123 Nowhere St, Imaginary City, ZZ</Caption>
+              <Caption>Email : email@example.com</Caption>
+              <Caption>Phone : +00 1234-5678</Caption>
+            </div>
+            <br />
+            <BodyOne>Subscribe To Our Newsletter</BodyOne>
+            <input
+              type="text"
+              className="p-3 w-full border bg-white-100 border-gray-300 rounded-md outline-none"
+              placeholder="email@example.com"
+            />
           </div>
         </div>
       </footer>
