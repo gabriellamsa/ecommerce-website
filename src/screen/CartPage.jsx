b0a7b9a7f2e597649c7fdbe9927cd405bdf2d9cd
gabriellamsa <gabii.marcelly@gmail.com>
2025-02-19 16:17:48 -0500

    feat: add CartPage content

--- src/screen/CartPage.jsx
+++ src/screen/CartPage.jsx
@@ -1,3 +1,21 @@
+import { useSelector, useDispatch } from "react-redux";
+import { selectTotalPrice } from "../redux/slice/cartSlice";
+import BgImage from "../assets/common/Frame.png";
+
 export const CartPage = () => {
-  return <div>CartPage</div>;
+  const cartItems = useSelector((state) => state.cart.itemList);
+  const totalPrice = useSelector(selectTotalPrice);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <section className="mt-16">
+        <div className="h-[50vh]">
+          <div className="images absolute top-0 left-0 w-full h-1/2">
+            <img src={BgImage} alt="" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
